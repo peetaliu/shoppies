1765e3cd4bfa38d92bb3a67c8bb38f2392d92e19
Peter <peterdotliu@gmail.com>
2020-08-28 23:50:01 -0400

    Add basic component structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { useState, useEffect } from 'react'
+import Search from './components/Search/Search'
+import Results from './components/Results/Results'
+import Nominations from './components/Nominations/Nominations'
 import './App.css'
 
 const App = () => {
-  return <h1>Shoppies</h1>
+  const [search, setSearch] = useState('')
+  const [results, setResults] = useState([])
+  const [nominations, setNominations] = useState([])
+
+  return (
+    <div>
+      <h1>Shoppies</h1>
+      <Search />
+      <div>
+        <Results />
+        <Nominations />
+      </div>
+    </div>
+  )
 }
 
 export default App
