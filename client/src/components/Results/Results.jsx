47b509b065d9835ceaa904da1b2c84283f4470f0
Peter <peterdotliu@gmail.com>
2020-08-29 16:58:34 -0400

    Add basic display of search results

--- client/src/components/Results/Results.jsx
+++ client/src/components/Results/Results.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
 const Results = props => {
-  return <div>Results</div>
+  return (
+    <div>
+      Results
+      <ul>
+        {props.results.map(m => (
+          <li key={m.Id}>
+            Title: {m.Title}
+            Year: {m.Year}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Results
