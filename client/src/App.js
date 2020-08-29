95bc93081bfd7b5316f6670f4ed68c7b3cea4f9b
Peter <peterdotliu@gmail.com>
2020-08-29 16:54:33 -0400

    Refractor search logic into its own component

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Search from './components/Search/Search'
 import Results from './components/Results/Results'
 import Nominations from './components/Nominations/Nominations'
+
 import './App.css'
 
 const App = () => {
-  const [search, setSearch] = useState('')
   const [results, setResults] = useState([])
   const [nominations, setNominations] = useState([])
+
+  const addNominations = e => {}
 
   return (
     <div>
       <h1>Shoppies</h1>
-      <Search />
+      <Search setResults={setResults} />
       <div>
-        <Results />
-        <Nominations />
+        <Results results={results} />
+        <Nominations nominations={nominations} />
       </div>
     </div>
   )
--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import omdbService from '../../services/omdbService'
 
 const Search = props => {
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    const getResults = async () => {
+      props.setResults([])
+      if (search.trim()) {
+        const result = await omdbService.search({ search: search.trim() })
+        console.log('result: ', result)
+
+        result.Search &&
+          props.setResults(
+            result.Search.map(m => ({
+              Title: m.Title,
+              Year: m.Year,
+              Id: m.imdbID,
+            }))
+          )
+      }
+    }
+    getResults()
+  }, [search])
   return (
     <div>
       <h3>Movie Title</h3>
-      <input value={props.search} onChange={props.handler} />
+      <input
+        type='text'
+        value={search}
+        name='Search'
+        onChange={({ target }) => setSearch(target.value)}
+        id='search'
+      />
     </div>
   )
 }
