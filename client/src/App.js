a971bc7e57f3700a284e7086c69b7c30ceb778ca
Peter <peterdotliu@gmail.com>
2020-08-30 21:18:35 -0400

    Implement notification functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './components/Search/Search'
-import Results from './components/Results/Results'
+import ResultList from './components/ResultList/ResultList'
 import Nominations from './components/Nominations/Nominations'
+import Notification from './components/Notification/Notification'
 
 import './App.css'
 
 const App = () => {
   const [results, setResults] = useState([])
   const [nominations, setNominations] = useState([])
+  const [notif, setNotif] = useState(null)
 
-  const addNominations = e => {}
+  useEffect(() => {
+    if (nominations.length === 5) {
+      setNotif('5 noms')
+      setTimeout(() => {
+        setNotif(null)
+      }, 5000)
+    }
+  }, [nominations])
 
   return (
     <div>
+      <Notification message={notif} />
       <h1>Shoppies</h1>
       <Search setResults={setResults} />
       <div>
-        <Results results={results} />
-        <Nominations nominations={nominations} />
+        <ResultList
+          results={results}
+          setNominations={setNominations}
+          nominations={nominations}
+        />
+        <Nominations
+          nominations={nominations}
+          setNominations={setNominations}
+        />
       </div>
     </div>
   )
