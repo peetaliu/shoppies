0a0c6b6cc8019cf3595e81c3ad684c6ef670e8f2
Peter <peterdotliu@gmail.com>
2020-08-30 21:19:42 -0400

    Implement nominations functionality

--- client/src/components/Nominations/Nominations.jsx
+++ client/src/components/Nominations/Nominations.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
-const Nominations = props => {
-  return <div>Nominations</div>
+const Nominations = ({ nominations, setNominations }) => {
+  return (
+    <div>
+      Nominations
+      <ul>
+        {nominations.map(n => (
+          <li key={n.Id}>
+            Title: {n.Title} <br />
+            Year: {n.Year}
+            <button
+              onClick={() =>
+                setNominations(nominations.filter(nom => nom.Id !== n.Id))
+              }>
+              Remove Nomination
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Nominations
