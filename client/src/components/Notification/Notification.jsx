4b6f0523ed2dd9f82ccaf9bfa97fe17785fd0522
Peter <peterdotliu@gmail.com>
2020-08-31 23:24:47 -0400

    Include bootstrap Alert component

--- client/src/components/Notification/Notification.jsx
+++ client/src/components/Notification/Notification.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Alert from 'react-bootstrap/Alert'
 
 const Notification = ({ message }) => {
   if (message === null) {
     return null
   }
 
-  return <div>{message}</div>
+  return <Alert variant='primary'>{message}</Alert>
 }
 
 export default Notification
